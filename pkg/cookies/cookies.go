8f9890a87d88e2a588c63d418926ddd7a559386c
irg1008 <ivansudevlop@gmail.com>
2023-09-10 20:27:11 +0200

    FORMATTER

--- pkg/cookies/cookies.go
+++ pkg/cookies/cookies.go
@@ -7,13 +7,6 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-const (
-	cookieName       = "refresh_token"
-	refreshTokenPath = "/api/auth/refresh"
-	loggedCookieName = "is_logged"
-	loggedCookiePath = "/"
-)
-
 func newCookie(name string, path string, age time.Duration) *http.Cookie {
 	return &http.Cookie{
 		Name:     name,
